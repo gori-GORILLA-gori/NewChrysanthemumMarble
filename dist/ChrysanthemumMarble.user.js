3b585d9aaa494c9c2e16373021e8e6c5ed21b830
gori-GORILLA-gori <gorillasub777@gmail.com>
2025-09-07 11:15:51 +0900

    _

--- dist/ChrysanthemumMarble.user.js
+++ dist/ChrysanthemumMarble.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         ChrysanthemumMarble
-// @namespace    https://github.com/Seris0/
+// @namespace    https://github.com/gori-GORILLA-gori
 // @version      0.89.5
 // @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
 // @author       Seris0
 // @license      MPL-2.0
-// @supportURL   https://discord.gg/tpeBPy46hf
-// @homepageURL  https://github.com/Seris0/Wplace-BlueMarble
+// @supportURL   https://github.com/gori-GORILLA-gori/NewChrysanthemumMarble/raw/refs/heads/main/dist/ChrysanthemumMarble.user.js
+// @homepageURL  https://github.com/gori-GORILLA-gori/NewChrysanthemumMarble/raw/refs/heads/main/dist/ChrysanthemumMarble.user.js
 // @icon         https://raw.githubusercontent.com/gori-GORILLA-gori/NewChrysanthemumMarble/refs/heads/main/dist/assets/Favicon.png
-// @updateURL    https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/main/dist/BlueMarble.user.js
-// @downloadURL  https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/main/dist/BlueMarble.user.js
+// @updateURL    https://github.com/gori-GORILLA-gori/NewChrysanthemumMarble/raw/refs/heads/main/dist/ChrysanthemumMarble.user.js
+// @downloadURL  https://github.com/gori-GORILLA-gori/NewChrysanthemumMarble/raw/refs/heads/main/dist/ChrysanthemumMarble.user.js
 // @run-at       document-start
 // @match        *://*.wplace.live/*
 // @grant        GM_getResourceText
@@ -20,7 +20,7 @@
 // @connect      nominatim.openstreetmap.org
 // @connect      wplace.live
 // @connect      wplace.lol
-// @resource     CSS-BM-File https://raw.githubusercontent.com/Seris0/Wplace-BlueMarble/cc70f8da7b10bb1c90f8387b38a659cbb917c80d/dist/BlueMarble.user.css
+// @resource     CSS-BM-File https://github.com/gori-GORILLA-gori/NewChrysanthemumMarble/raw/refs/heads/main/dist/ChrysanthemumMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://wplace.live
